83c2fea785570a824b6655c8c777a1144f61a466
allison-nau <anau@bu.edu>
2021-03-24 18:55:57 -0400

    Update deseq2_anau.R

--- Programmer/deseq2_anau.R
+++ Programmer/deseq2_anau.R
@@ -3,19 +3,21 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
 BiocManager::install("DESeq2")
+BiocManager::install("apeglm")
 
 
 # Import libraries:
 library(tidyverse)
 library(DESeq2)
+library("apeglm")
 
 # Read in csv as dataframes:
 temp_ds_exp <- read.csv("featureCounts_combined.csv", header=TRUE)
 temp_ds_ctrl <- read.csv("control_counts.csv", header=TRUE)
 
 # Convert to tibbles:
-temp_tib_exp <- as.tibble(temp_ds_exp)
-temp_tib_ctrl <- as.tibble(temp_ds_ctrl)
+temp_tib_exp <- as_tibble(temp_ds_exp)
+temp_tib_ctrl <- as_tibble(temp_ds_ctrl)
 
 #Add prefix to control samples:
 colnames(temp_tib_ctrl) <- paste("ctrl", colnames(temp_tib_ctrl), sep=".")
@@ -24,44 +26,54 @@ names(temp_tib_ctrl)[names(temp_tib_ctrl)=="ctrl.Geneid"] <- "Geneid"
 
 # Join the two tibbles, keeping all the rows in the sample dataset (tib1)
 # but not all rows in the control dataset (tib2)
+#TODO: do full_join instead?
 tib_comb1 <- left_join(temp_tib_exp, temp_tib_ctrl, by="Geneid")
 
 # filter out rows that have any zeros for funzies
 tib_comb2 <- subset(tib_comb1,rowSums(tib_comb1==0)==0)
 
+#Read in Metadata dataframe:
+# TODO: replace with our actual file:
+meta <- read.csv("group_EX_rna_info.csv", header=TRUE)
+# Convert mode_of_action to factor variable:
+meta$mode_of_action <- factor(meta$mode_of_action)
 
+# Keep only the samples in meta:
+# TODO: modify for the multiple groups!!!!
+control_list <- meta$Run[meta$mode_of_action=="Control"] 
+print("Control sample list:")
+control_list
+exp_list <- meta$Run[meta$mode_of_action!="Control"]
+print("Experimental sample list:")
+exp_list
+full_list <- meta$Run
 
-# # # Deseq example shell script:~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#R
+# Subset big dataframe and only keep ones I want
+tib_comb_subset <- subset(tib_comb2, select=c("Geneid", full_list))
 
+# Convert Geneids to rownames:
+tib_comb_subset <- column_to_rownames(tib_comb_subset, var="Geneid")
 
-
-# load counts
-cnts <- read.csv('groups/group_EX_rna_counts.csv',row.names=1)
-
-# filter out rows that have any zeros for funzies
-cnts <- subset(cnts,rowSums(cnts==0)==0)
-
-# sample information
-info <- read.csv('groups/group_EX_rna_info.csv')
 
 # create the DESeq object
-dds <- DESeqDataSetFromMatrix(
-  countData = cnts,
-  colData = info,
+dds1 <- DESeqDataSetFromMatrix(
+  countData = tib_comb_subset,
+  colData = meta,
   design= ~ mode_of_action
 )
 
 # relevel mode_of_action as factor
-dds$mode_of_action <- relevel(dds$mode_of_action, ref='Control')
+dds1$mode_of_action <- relevel(dds1$mode_of_action, ref='Control')
 
 # run DESeq
-dds <- DESeq(dds)
-res <- results(dds, contrast=c('mode_of_action','HMGCOA','Control'))
-res <- lfcShrink(dds, coef=2)
+dds1 <- DESeq(dds1)
+# Adjust for our actual samples:
+res1 <- results(dds1, contrast=c('mode_of_action','HMGCOA','Control'))
+res1 <- lfcShrink(dds1, coef=2)
 
 # write out DE results
-write.csv(res,'example_deseq_results.csv')
+write.csv(res1,'example_deseq_results1.csv')
 
 # write out matrix of normalized counts
-write.csv(counts(dds,normalized=TRUE),'example_deseq_norm_counts.csv')
+write.csv(counts(dds1,normalized=TRUE),'example_deseq_norm_counts1.csv')
+
