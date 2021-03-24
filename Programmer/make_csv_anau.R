9a8ab602c7b10f39e1674e68c89cfab8ba64baa6
allison-nau <anau@bu.edu>
2021-03-23 20:54:23 -0400

    Create make_csv_anau.R

---
+++ Programmer/make_csv_anau.R
@@ -0,0 +1,29 @@
+# Combines featureCounts_output files into one csv file
+
+# Import libraries:
+library(tidyverse)
+
+# Get list of applicable file names:
+all_files <- list.files(pattern="^featureCounts_output.*txt$")
+all_files
+num_files <- length(all_files)
+
+# For every file:
+for (i in 1:num_files){
+  my_file <- all_files[i]
+  # Read in table:
+  temp_ds <- read.table(my_file, header=TRUE)
+  # Grab 1st (should be Geneid) and last (counts in Sample)
+  temp_ds <- temp_ds[,c(1, ncol(temp_ds))]
+  # Convert to tibble
+  temp_tib <- as_tibble(temp_ds)
+  # Start combined tibble if first file, otherwise merge with existing combined tibble:
+  if (i==1){
+    comb_tib <- temp_tib
+  }else{
+    comb_tib <- full_join(comb_tib, temp_tib, by="Geneid")
+  }
+}
+
+# Write csv:
+write.csv(comb_tib, file="featureCounts_combined.csv", row.names=FALSE)
