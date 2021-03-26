96a890abae34b94634aa53d2fc3a069e57990e9a
allison-nau <anau@bu.edu>
2021-03-26 00:12:57 -0400

    Update run_featurecounts_anau.sh
    20210325 Updated for actual samples

--- Programmer/run_featurecounts_anau.sh
+++ Programmer/run_featurecounts_anau.sh
@@ -8,8 +8,11 @@ source /etc/bashrc
 
 # This script will run featureCounts for samples stored in specified folder.
 
-# When running: qsub run_featurecounts_anau.sh
+# To run: qsub run_featurecounts_anau.sh
 # Check status: qstat -u anau
+
+# Star output files:
+# /projectnb/bf528/users/dachshund/project_3/samples/STAR_output
 
 
 # Load module:
@@ -19,19 +22,19 @@ module load subread/1.6.2
 GTF=/project/bf528/project_3/reference/rn4_refGene_20180308.gtf
 
 # For every applicable file in our folder:
-for file in /project/bf528/project_3/samples/*.bam 
-# for file in /projectnb2/bf528/users/dachshund/project_3/programmer/samples/star_output/*Aligned.sortedByCoord.out.bam
+for file in /projectnb2/bf528/users/dachshund/project_3/samples/STAR_output/*Aligned.sortedByCoord.out.bam
 
-# Do the following:
 do
+
 # Create a shortened filename:
 f=$(basename “$file”)
 f=${f:0:10}
 
 # Run the following command:
-# TODO: do I have the right arguments??
+# Some of the default arguments used:
+## minOverlap: 1 overlapping base by default
 echo $f
-OUT=/projectnb2/bf528/users/dachshund/project_3/programmer/featureCounts_output$f.txt
+OUT=/projectnb2/bf528/users/dachshund/project_3/programmer/featureCounts_ourSamples/featureCounts_output$f.txt
 featureCounts -T 16 -a $GTF -o $OUT $file
 
 # End for loop
