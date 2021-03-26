0f55cdd7e4981f442aaf6bbc4d03fb5fb8c192a1
allison-nau <anau@bu.edu>
2021-03-26 00:12:33 -0400

    Update deseq2_anau.R
    20210325 Updated for actual samples, and to confirm works in command line for R scripts

--- Programmer/deseq2_anau.R
+++ Programmer/deseq2_anau.R
@@ -1,3 +1,9 @@
+# Runs deseq2_anau.R
+
+# To run from command line:
+# module load R/4.0.2
+# Rscript deseq2_anau.R
+
 # Install packages:
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
@@ -5,11 +11,15 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 BiocManager::install("DESeq2")
 BiocManager::install("apeglm")
 
+#Install hardhat:
+install.packages("hardhat", repos = "http://cran.us.r-project.org")
 
 # Import libraries:
 library(tidyverse)
 library(DESeq2)
 library("apeglm")
+library(hardhat)
+library(stringr)
 
 # Read in csv as dataframes:
 temp_ds_exp <- read.csv("featureCounts_combined.csv", header=TRUE)
@@ -19,27 +29,23 @@ temp_ds_ctrl <- read.csv("control_counts.csv", header=TRUE)
 temp_tib_exp <- as_tibble(temp_ds_exp)
 temp_tib_ctrl <- as_tibble(temp_ds_ctrl)
 
-#Add prefix to control samples:
-colnames(temp_tib_ctrl) <- paste("ctrl", colnames(temp_tib_ctrl), sep=".")
-#Remove prefix from Geneid col name:
-names(temp_tib_ctrl)[names(temp_tib_ctrl)=="ctrl.Geneid"] <- "Geneid"
+##To add prefix to control samples:
+#colnames(temp_tib_ctrl) <- paste("ctrl", colnames(temp_tib_ctrl), sep=".")
+##Remove prefix from Geneid col name:
+#names(temp_tib_ctrl)[names(temp_tib_ctrl)=="ctrl.Geneid"] <- "Geneid"
 
-# Join the two tibbles, keeping all the rows in the sample dataset (tib1)
-# but not all rows in the control dataset (tib2)
-#TODO: do full_join instead?
-tib_comb1 <- left_join(temp_tib_exp, temp_tib_ctrl, by="Geneid")
+# Join the two tibbles, keeping all the rows that are present in both controls and exp
+tib_comb1 <- inner_join(temp_tib_exp, temp_tib_ctrl, by="Geneid")
 
 # filter out rows that have any zeros for funzies
 tib_comb2 <- subset(tib_comb1,rowSums(tib_comb1==0)==0)
 
 #Read in Metadata dataframe:
-# TODO: replace with our actual file:
-meta <- read.csv("group_EX_rna_info.csv", header=TRUE)
+meta <- read.csv("toxgroup_1_rna_info.csv", header=TRUE)
 # Convert mode_of_action to factor variable:
 meta$mode_of_action <- factor(meta$mode_of_action)
 
 # Keep only the samples in meta:
-# TODO: modify for the multiple groups!!!!
 control_list <- meta$Run[meta$mode_of_action=="Control"] 
 print("Control sample list:")
 control_list
@@ -47,6 +53,13 @@ exp_list <- meta$Run[meta$mode_of_action!="Control"]
 print("Experimental sample list:")
 exp_list
 full_list <- meta$Run
+full_list
+
+# Grab mode of action factor levels:
+modes_of_action <- get_levels(meta)$mode_of_action
+modes_of_action
+exp_modes_of_action <- modes_of_action[modes_of_action!="Control"]
+exp_modes_of_action
 
 # Subset big dataframe and only keep ones I want
 tib_comb_subset <- subset(tib_comb2, select=c("Geneid", full_list))
@@ -54,26 +67,60 @@ tib_comb_subset <- subset(tib_comb2, select=c("Geneid", full_list))
 # Convert Geneids to rownames:
 tib_comb_subset <- column_to_rownames(tib_comb_subset, var="Geneid")
 
+# Run DESeq for each group:
+for (exp_group in exp_modes_of_action){
+  cat("===============================================================\n\n\n")
+  print("Experimental group:")
+  print(exp_group)
+  
+  # Get the experimental group sample names:
+  temp_exp_sample_list <- meta$Run[meta$mode_of_action==exp_group]
+  print("Sample list:")
+  print(temp_exp_sample_list)
+  
+  # Subset dataframe:
+  temp_tib <- tib_comb_subset[,c(temp_exp_sample_list, control_list)]
+  # Names of columns remaining:
+  print("Columns kept:")
+  print(names(temp_tib))
+  #Subset meta data:
+  temp_meta <- meta[meta$mode_of_action==exp_group | meta$mode_of_action=="Control",]
+  print("Meta data kept:")
+  print(temp_meta)
+  
+  # create the DESeq object
+  dds1 <- DESeqDataSetFromMatrix(
+    countData = temp_tib,
+    colData = temp_meta,
+    design= ~ mode_of_action
+  )
+  
+  # relevel mode_of_action as factor
+  dds1$mode_of_action <- relevel(dds1$mode_of_action, ref='Control')
+  
+  # run DESeq
+  dds1 <- DESeq(dds1)
+  # Adjust for our actual samples:
+  res1 <- results(dds1, contrast=c('mode_of_action', exp_group,'Control'))
+  res1 <- lfcShrink(dds1, coef=2)
 
-# create the DESeq object
-dds1 <- DESeqDataSetFromMatrix(
-  countData = tib_comb_subset,
-  colData = meta,
-  design= ~ mode_of_action
-)
+  # Strip out weird characters from exp groups:
+  exp_group_str <- str_replace_all(exp_group, "[[:punct:]]", "")
+  print(exp_group)
+  print("Striped group name:")
+  print(exp_group_str)
+  
+  # write out DE results
+  write.csv(res1, paste(exp_group_str, 'deseq_results1.csv', sep="_"))
+  
+  # write out matrix of normalized counts
+  write.csv(counts(dds1,normalized=TRUE),
+            paste(exp_group_str, 'deseq_norm_counts1.csv', sep="_"))
+}
 
-# relevel mode_of_action as factor
-dds1$mode_of_action <- relevel(dds1$mode_of_action, ref='Control')
 
-# run DESeq
-dds1 <- DESeq(dds1)
-# Adjust for our actual samples:
-res1 <- results(dds1, contrast=c('mode_of_action','HMGCOA','Control'))
-res1 <- lfcShrink(dds1, coef=2)
 
-# write out DE results
-write.csv(res1,'example_deseq_results1.csv')
 
-# write out matrix of normalized counts
-write.csv(counts(dds1,normalized=TRUE),'example_deseq_norm_counts1.csv')
+
+
 
